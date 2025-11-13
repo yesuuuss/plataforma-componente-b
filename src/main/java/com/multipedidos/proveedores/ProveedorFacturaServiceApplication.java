95ddd296ce63800d39d21c667bda0ebe16bdd8b1
yesuuuss <jesupalma9@gmail.com>
2025-11-13 17:39:19 -0600

    feat: se agregaron los componentes para el problema de cors

--- src/main/java/com/multipedidos/proveedores/ProveedorFacturaServiceApplication.java
+++ src/main/java/com/multipedidos/proveedores/ProveedorFacturaServiceApplication.java
@@ -2,12 +2,28 @@ package com.multipedidos.proveedores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ProveedorFacturaServiceApplication {
+public class ComponenteBApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProveedorFacturaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ComponenteBApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
